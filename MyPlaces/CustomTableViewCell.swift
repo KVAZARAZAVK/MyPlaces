363dc82d53a7521b2b31a216689af54230ebfe4a
Anatoly Valkov <kvazarazavk@gmail.com>
2020-06-08 23:38:48 -0400

    CustonTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Anatoly Valkov on 6/8/20.
+//  Copyright © 2020 Anatoly Valkov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
