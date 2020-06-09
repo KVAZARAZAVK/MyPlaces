49683c8157995a1830a39fdbd9fabf25baeae4fe
Anatoly Valkov <kvazarazavk@gmail.com>
2020-06-08 23:21:13 -0400

    App Icon Launch Screen Row Heigtht

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainViewController: UITableViewController {
     
+    
+    
     let restaurantNames = [
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
         "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
@@ -37,10 +39,17 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = restaurantNames[indexPath.row]
         cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
 
         return cell
     }
     
+    // MARK: - TableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
 
    
     /*
